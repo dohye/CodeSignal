306a0e65360cc63b29f4e72b8b18c7bf94dc47b3
dohye kim <dohyedohye@gmail.com>
2020-06-21 23:57:35 +0900

    Create 05-shapeArea.py

---
+++ 05-shapeArea.py
@@ -0,0 +1,10 @@
+# Description
+# Below we will define an n-interesting polygon. Your task is to find the area of a polygon for a given n.
+
+# solution
+def shapeArea(n):
+
+    return 1+(2*(n-1))*n
+       
+# [Copyright © 2020 BrainFights, Inc. All rights reserved.]
+# https://codesignal.com/
