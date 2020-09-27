58f04b5d17fe25fd56307b729011ffde2d91c668
dohye kim <dohyedohye@gmail.com>
2020-09-27 12:18:36 +0900

    Create 36-differentSymbolsNaive.py

---
+++ 36-differentSymbolsNaive.py
@@ -0,0 +1,11 @@
+# Description :
+"""
+Given a string, find the number of different characters in it.
+"""
+
+# Solution :
+def differentSymbolsNaive(s):
+    return len(set(s))
+
+# [Copyright © 2020 BrainFights, Inc. All rights reserved.]
+# https://codesignal.com/
